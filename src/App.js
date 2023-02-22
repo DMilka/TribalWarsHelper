95f9896d7ecb080b8a6d407e8cb5cb5eadf732d2
Dawid Milka <dawid.milka@interia.com>
2023-02-23 00:05:25 +0100

    [bug] Removed BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import MarchTimeCalc from "./pages/MarchTimeCalc/MarchTimeCalc";
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 function App() {
   return (
-      <BrowserRouter>
         <HashRouter basename={"/"}>
           <div style={{backgroundColor: '#2C394B'}}>
             <MarchTimeCalc/>
           </div>
         </HashRouter>
-      </BrowserRouter>
   );
 }
 
